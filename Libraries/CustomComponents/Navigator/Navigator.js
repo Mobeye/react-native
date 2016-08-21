5b1ce0cfe408f895c138206b7d6fd13c3861ee05
btoueg <benjamin@mobeye-app.com>
2016-08-21 20:12:24 +0200

    Reverse scene animation

--- Libraries/CustomComponents/Navigator/Navigator.js
+++ Libraries/CustomComponents/Navigator/Navigator.js
@@ -1347,7 +1347,7 @@ var Navigator = React.createClass({
           onResponderTerminationRequest={
             this._handleResponderTerminationRequest
           }>
-          {scenes}
+          {this.props.reverse ? scenes.reverse() : scenes}
         </View>
         {this._renderNavigationBar()}
       </View>
